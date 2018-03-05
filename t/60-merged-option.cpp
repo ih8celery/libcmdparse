418cdad53f09853b814a023a5e418f3fe2d13275
Adam Marshall <adamm3992@gmail.com>
2018-03-04 19:30:41 -0500

    add simple tests for bsd and merged options

---
+++ t/60-merged-option.cpp
@@ -0,0 +1,51 @@
+/*
+ * 60-merged-option.cpp
+ *
+ * test the use of merged options
+ */
+
+#define WANT_TEST_EXTRAS
+#include <tap++.h>
+#include "options_parsing.h"
+
+using namespace TAP;
+using namespace util;
+
+int main() {
+  opt_parser p;
+
+  p.set(config_constants::merged_opt);
+
+  p.option("d?", "due");
+  p.option("a", "awe");
+  auto opt = p.option("f*", "file");
+
+  char ** argv = new char*[2];
+
+  argv[0] = (char *)"--afd";
+  argv[1] = (char *)"f";
+
+  opt_info info;
+  
+  try {
+    info = p.parse(argv, 2);
+  }
+  catch (const parse_error& e) {
+    std::cout << e.what() << std::endl;
+
+    return 0;
+  }
+
+  delete [] argv;
+
+  plan(4);
+
+  ok(info.has("file"), "found file argument");
+  ok(info.count("file") == 2, "file argument twice");
+  ok(info.count("awe") == 1, "found awe argument once");
+  ok(info.count("due") == 1, "found due argument once");
+
+  done_testing();
+
+  return exit_status();
+}
---
+++ t/70-bsd-option.cpp
@@ -0,0 +1,49 @@
+/*
+ * 60-merged-option.cpp
+ *
+ * test the use of merged options
+ */
+
+#define WANT_TEST_EXTRAS
+#include <tap++.h>
+#include "options_parsing.h"
+
+using namespace TAP;
+using namespace util;
+
+int main() {
+  opt_parser p;
+
+  p.set(config_constants::bsd_opt);
+
+  p.option("d?", "due");
+  p.option("a", "awe");
+  p.option("f*", "file");
+
+  char ** argv = new char*;
+
+  argv[0] = (char *)"afd";
+
+  opt_info info;
+  
+  try {
+    info = p.parse(argv, 1);
+  }
+  catch (const parse_error& e) {
+    std::cout << e.what() << std::endl;
+
+    return 0;
+  }
+
+  delete argv;
+
+  plan(3);
+
+  ok(info.has("file"), "found file argument");
+  ok(info.count("awe") == 1, "found awe argument once");
+  ok(info.count("due") == 1, "found due argument once");
+
+  done_testing();
+
+  return exit_status();
+}
